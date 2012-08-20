0b097383adbc524610fef1eb694883588e4ec584
Nick O'Neill <mr.nick.oneill@gmail.com>
2012-08-20 10:23:14 -0700

    Checking in a blank rails project

---
+++ README.md
@@ -0,0 +1,5 @@
+**Recurly Rails**
+
+I went searching for some sample rails integrations with Rails and couldn't find one.  So, I've built my own.  
+
+Once this is completed, you should be able to simply fork and clone the project and have recurly up and running with your own pricing page, payment page, and login integration.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecurlyRails::Application.config.secret_token = '8b11225275666fb27795a82a534463a79be9c682e5c85655f42c78810b24c14d52bbf591df16d196389855255f28c1ea337561b0d3fe131d98579542af8411cb'
