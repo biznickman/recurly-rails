852ce7691220e78b1ceb4034fa2ddb9269dbf3fc
Nick O'Neill <mr.nick.oneill@gmail.com>
2012-08-23 10:48:37 -0700

    Updating config settings based on this: http://stackoverflow.com/questions/7275636/rails-3-1-0-actionviewtemplateerrror-application-css-isnt-precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ RecurlyRails::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -28,7 +28,7 @@ RecurlyRails::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
